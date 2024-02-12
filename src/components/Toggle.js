3959b5246e4e9237611c67020cc821da8d610c98
Makena Sandra <47531164+makenasandra@users.noreply.github.com>
2024-02-12 06:27:15 +0000

    added state

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  const [status, setStatus] = useState({});
+  const [count, setCount] = useState(0);
+
+  function handleButtonClick(){
+    
+      setIsOn(isOn => {return !isOn});
+      setStatus(status => {return {...status, "status": isOn}} )
+    setCount(count => count + 2)
+      // console.log({isOn, })
+  }
+  console.log({isOn, status, count })
+  const color = isOn ? "red" : "white";
+  return <button style={{ background: color }} onClick={handleButtonClick}>{isOn? "ON":"OFF"}</button>;
 }
 
 export default Toggle;
